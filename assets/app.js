7c4fcb594b5b10c5b00b5dfbcce13cb90465f7b4
AnaSalazar <anastephany.salazarguzman@gmail.com>
2017-07-13 22:42:24 -0500

    Mostrando datos de los 6 exoplanetas

--- assets/app.js
+++ assets/app.js
@@ -17,13 +17,13 @@ getJSON("data/earth-like-results.json")
     exoplanetas.forEach(function(exoplaneta){
       getJSON(exoplaneta)
         .then(function(exoplaneta){
-          return (exoplaneta)
+          console.log(exoplaneta)
+          tarjeta(exoplaneta);
         })
-        tarjeta();
     })
   })
 
-var tarjeta = function(exoplaneta){
+function tarjeta (exoplaneta) {
   var divRow = document.createElement("div");
   var divCol = document.createElement("div");
   var divCard = document.createElement("div");
@@ -34,13 +34,25 @@ var tarjeta = function(exoplaneta){
   var dato2 = document.createElement("p");
 
   divRow.className = "row";
-  divCol.className = "col s12";
+  divCol.className = "col s12 offset-m3 m6 center";
   divCard.className = "card blue-grey darken-1";
   divCardContent.className = "card-content white-text";
   imagen.className = "s8";
   nombre.className = "card-title";
-  // dato1.className = "card-content white-text";
-  // dato2.className = "card-content white-text";
-}
 
-// document.onready()
+  imagen.src = "archivos/exoplanetas.jpg"
+
+  nombre.innerText = exoplaneta.pl_name;
+  dato1.innerText = "ORBPER: " + exoplaneta.pl_orbper;
+  dato2.innerText = "TELESCOPE: " + exoplaneta.pl_telescope;
+
+  var espacio = document.getElementById("espacioExoplanetas");
+  divCardContent.appendChild(imagen);
+  divCardContent.appendChild(nombre);
+  divCardContent.appendChild(dato1);
+  divCardContent.appendChild(dato2);
+  divCard.appendChild(divCardContent);
+  divCol.appendChild(divCard);
+  divRow.appendChild(divCol);
+  espacio.appendChild(divRow);
+}
